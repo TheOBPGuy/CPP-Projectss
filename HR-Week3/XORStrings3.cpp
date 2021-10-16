25855bed44edb6c5159a8f4cb53542e3a2ba734e
TheOBPGuy <f.ibrahim.obp@gmail.com>
2021-10-15 20:44:23 -0400

    XOR String 3

---
+++ HR-Week3/XORStrings3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string strings_xor(string s, string t) {
+    string res = "";
+    int size = s.size();
+    for (int i = 0; i < size; i++) {
+        if (s[i] == '0' && t[i] == '0') {
+            res += "0";
+        } else if (s[i] == '0' && t[i] == '1') {
+            res += "1";
+        } else if (s[i] == '1' && t[i] == '0') {
+            res += "1"; 
+        } else {
+            res += "0";
+        }
+    }
+    return res;
+}
+
+int main() {
+    return 0;
+}

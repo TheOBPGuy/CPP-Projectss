5f15466fb8d59e609eac8352513de83300fe32e3
TheOBPGuy <f.ibrahim.obp@gmail.com>
2021-10-24 23:57:58 -0400

    Allocate Books - Final Update

--- BinarySearch/AllocateBlocks.cpp
+++ BinarySearch/AllocateBlocks.cpp
@@ -3,8 +3,27 @@
 
 using namespace std;
 
-bool isPossible(vector<int> A, int num) {
+bool isPossible(vector<int> A, int num, int mid);
+int books(vector<int> A, int B);
 
+bool isPossible(vector<int> A, int num, int mid) {
+    int newSum = 1;
+    int tempSum = 0;
+    for (int i = 0 ; i< A.size(); i++) {
+        tempSum += A[i];
+        if (tempSum > mid) {
+            // If we are crossing the mid then we need to allocate to another student
+            newSum++;
+            tempSum = 0;
+            i--;
+        }
+    }
+    // If we crossed over the 
+    if (newSum > num) {
+        return false;
+    } else {
+        return true;
+    }
 }
 
 int books(vector<int> A, int B) {
@@ -23,15 +42,22 @@ int books(vector<int> A, int B) {
         mid = lo + (hi-lo)/2;
         // 2. Iterate from start. check how many books can be alloted
         // to student without crossing the 
+        cout << mid << "\n";
+        if (isPossible(A,B,mid)) {
+            hi = mid-1;
+            res = mid;
+        } else {
+            lo = mid+1;
+        }
     }
 
-    return -1;
+    return res;
 }
 
 int main() {
-    vector<int> A = {12, 34, 67, 90};
-    // vector<int> A = {73, 58, 30, 72, 44, 78, 23, 9};
-    int B = 2;
+    // vector<int> A = {12, 34, 67, 90};
+    vector<int> A = {73, 58, 30, 72, 44, 78, 23, 9};
+    int B = 5;
     cout << "\n";
     cout << books(A,B);
     return 0;

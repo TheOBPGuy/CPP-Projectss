9f102c9976c882d62cb3ef42c5556ab5c68793b3
TheOBPGuy <f.ibrahim.obp@gmail.com>
2021-10-29 19:40:09 -0400

    Median of Two Sorted Arrays

---
+++ BinarySearch/MedianOfArray.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+double findMedianSortedArrays(vector<int> A, vector<int> B) {
+    // 1 2 3 4 5
+    // 2 3
+    // 1 4 5
+    if (B.size() < A.size()) {
+        // We need to use the smaller Array to get the partition
+        return findMedianSortedArrays(B,A);
+    }
+    int n1 = A.size(), n2 = B.size();
+    int lo = 0, hi = n1-1;
+
+    while (lo <= hi) {
+        int cut1 = lo+(hi-lo)/2;
+        int cut2 = ((n1+n2+1)/2) - cut1;
+
+        int left1 = cut1 == 0 ? INT_MIN : A[cut1-1];
+        int left2 = cut2 == 0 ? INT_MIN : A[cut2-1];
+
+        int right1 = cut1 == 0 ? INT_MAX : A[cut1];
+        int right2 = cut2 == 0 ? INT_MAX : A[cut2];
+
+        if (left1 > right2) {
+            hi = cut1-1;
+        } else if (left2 > right1) {
+            lo = cut1+1;
+        } else {
+            return (n1+n2)%2 == 0 ? (max(left1, left2) + min(right1, right2))/2 : max(left1,left2);
+        }
+    }
+
+}
+
+int main() {
+    return 0;
+}

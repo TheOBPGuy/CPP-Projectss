88b22e0ac02467bee367458ee735422d8db9bd5b
TheOBPGuy <f.ibrahim.obp@gmail.com>
2021-11-06 23:15:11 -0400

    Maximum Absolute Difference

--- Arrays/MaximumAbsoluteDifference.cpp
+++ Arrays/MaximumAbsoluteDifference.cpp
@@ -4,22 +4,20 @@
 using namespace std;
 
 int maxArr(vector<int> A) {
-    int currentSum = 0;
-    int maxSum = INT_MIN;
-    // 1 3 -1
-    for (int i = 0; i < A.size(); i++) {
-        int func = 0;
-        if (i == 0) {
-            // We are looking at the first entry 
-            currentSum = 0;
-            
-        } else {
-            currentSum = abs(A[i]-A[i-1]) + abs(i-(i-1));
-            
-        }
-        maxSum = max(maxSum,currentSum);
+    int max1 = INT_MIN;
+    int max2 = INT_MIN;
+    int min1 = INT_MAX;
+    int min2 = INT_MAX;
+
+    for (int i = 0 ; i < A.size(); i++) {
+        max1 = max(max1, A[i] + i);
+        max2 = max(max2, -A[i] + i);
+
+        min1 = min(min1, A[i] + i);
+        min2 = min(min2, -A[i] + i);
     }
-    return maxSum;
+    int max_ans = max(max1-min1, max2-min2);
+    return max_ans;
 }
 
 int main() {

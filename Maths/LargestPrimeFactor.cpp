a7e54a0134bddec36a0d22c83a68a05ececbf664
TheOBPGuy <f.ibrahim.obp@gmail.com>
2021-11-11 09:22:33 -0500

    Largest Prime Factor - Most Optimized

--- Maths/LargestPrimeFactor.cpp
+++ Maths/LargestPrimeFactor.cpp
@@ -19,6 +19,27 @@ long long int solve(long long int num) {
     return res;
 }
 
+long long int solution2(long long int num) {
+    int maxPrime = 0, oddPrime = 3;
+
+    // Cancel out the evens at first
+    while (num % 2 == 0) {
+        num = num / 2;
+        maxPrime = 2;
+    }
+
+    while (num != 1) {
+        while (num % oddPrime == 0) {
+            num = num / oddPrime;
+            maxPrime = oddPrime;
+        }
+        oddPrime += 2;
+    }   
+
+    return maxPrime;
+
+}
+
 bool isPrime(long long int N) {
     if (N <= 1) { return false; }
     for (int i = 2; i*i <= N; i++) {
